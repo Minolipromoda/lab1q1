964499c9faa5ac5e57f9ffe3c7bc0eee565630a2
MINOLI <MINOLI@LAPTOP-VTQD26BJ>
2024-02-04 23:33:28 +0530

    Add project files.

---
+++ lab1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace  lab1
+{
+    internal class program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine(" enter the length of the rectangle :");
+            double length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter the width of the rectangle:");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            double area = length * width;
+            Console.WriteLine($"The area of the rectangle is : {area}");
+            Console.ReadLine();
+        }
+    }      
+}
